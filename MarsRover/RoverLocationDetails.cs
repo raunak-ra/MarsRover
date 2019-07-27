b3b9689ab0deece07bc2bf2b052149a2c5dec58b
tavisca-rsingh <rsingh@tavisca.in>
2019-07-27 20:57:21 +0530

    Removed Comment

--- MarsRover/RoverLocationDetails.cs
+++ MarsRover/RoverLocationDetails.cs
@@ -9,12 +9,7 @@ namespace MarsRover
     public class RoverLocationDetails
     {
         public int XCurrent, YCurrent, XLower=0, YLower=0, XUpper, YUpper;
-        //public int YCurrent { get; set; }
-        //public int XLower { get; set; }
-        //public int YLower { get; set; }
-        //public int XUpper { get; set; }
-        //public int YUpper { get; set; }
-        public string RoverDirection; /*{ get; set; }*/
+        public string RoverDirection; 
 
         public RoverLocationDetails(int xcurrent, int ycurrent, int xupper, int yupper, string direction)
         {
