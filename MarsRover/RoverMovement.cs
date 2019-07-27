ccc02d0aafcc30a225eb6fae53609645b6ba5e6c
tavisca-rsingh <rsingh@tavisca.in>
2019-07-27 12:49:49 +0530

    Refactored turn and move

--- MarsRover/RoverMovement.cs
+++ MarsRover/RoverMovement.cs
@@ -8,81 +8,15 @@ namespace MarsRover
 {
    public class RoverMovement 
     {
-        
+        private Turn turn;
+        private Move move;
 
         public void PerformAction(char key, RoverPosition position)
         {
-
-            switch(key)
-            {
-                case 'F':
-                    MoveForward(position); break;
-                case 'L':
-                    TurnLeft(position); break;
-                case 'R':
-                    TurnRight(position); break;
-                default:
-                    throw new Exception("Invalid Key");
-
-            }
+            if (key == 'L' || key == 'R') turn.PerformTurn(position, key);
+            else if (key == 'M') move.MoveForward(position);
         }
 
-        List<string> Direction = new List<string>() { "north","northeast", "east", "southeast", "south", "southwest", "west", "northwest" };
-
-        private void TurnRight(RoverPosition position)
-        {
-            position.RoverDirection = Direction[((Direction.IndexOf(position.RoverDirection) + 1) % 8)];
-        }
-
-        private void TurnLeft(RoverPosition position)
-        {
-            position.RoverDirection = Direction[((Direction.IndexOf(position.RoverDirection) + 7) % 8)];
-        }
-
-        public void MoveForward(RoverPosition position)
-        {
-            BoundaryCheckor boundarycheckor = new BoundaryCheckor();
-            if(boundarycheckor.CheckBoundary(position))
-            {
-                switch(position.RoverDirection)
-                {
-                    case "north":
-                        position.YCurrent += 1;
-                        break;
-                    case "south":
-                        position.YCurrent -= 1;
-                        break;
-                    case "east":
-                        position.XCurrent += 1;
-                        break;
-                    case "west":
-                        position.XCurrent -= 1;
-                        break;
-                    case "northeast":
-                        position.YCurrent += 1;
-                        position.XCurrent += 1;
-                        break;
-                    case "southwest":
-                        position.XCurrent -= 1;
-                        position.YCurrent -= 1;
-                        break;
-                    case "southeast":
-                        position.XCurrent += 1;
-                        position.YCurrent -= 1;
-                        break;
-                    case "northwest":
-                        position.XCurrent -= 1;
-                        position.YCurrent += 1;
-                        break;
-
-                }
-
-            }
-            else
-            {
-               // DO NOT MOVE !!!!
-            }
-        }
-
+      
     }
 }
